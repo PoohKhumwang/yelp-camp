f2ed2c74f84329296ab06f5af84ccce212901f35
pooh <6330423221@student.chula.ac.th>
2025-09-14 18:13:25 +0700

    port

--- app.js
+++ app.js
@@ -160,6 +160,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-app.listen(port, () => console.log("Serving on port 3000"));
+app.listen(port, () => console.log(`Serving on port ${port} `));
